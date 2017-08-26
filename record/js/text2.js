a0f20237268c21e156b3f19de082b62225375eca
Tigerless <1943789151@qq.com>
2017-08-26 09:55:36 +0800

    debug

--- record/js/text2.js
+++ record/js/text2.js
@@ -14,7 +14,7 @@ function animate(element,target){
 }
 window.onload =function(){
     var ul = document.getElementsByTagName('ul')[0];
-    var spanArr = document.getElementsByTagName('span');
+    var spanArr = document.getElementsByClassName('control').item(0).getElementsByTagName('span');
     //4.最后一张显示后，克隆第一张图片继续显示第一张
     ul.appendChild(ul.children[0].cloneNode(true));
     var ulLeft = ul.offsetLeft;
@@ -24,12 +24,12 @@ window.onload =function(){
     //开启
     var autoPlayTimer = setInterval(function(){
         //4.2 从最后一张滚回到第一张(ulIndex == 5)之后  需重置回第一张状态
-        if(ulIndex == 5){
+        if(ulIndex == 3){
             ulIndex = 0;
             ul.style.left = '0';
             ulLeft = 0;
         }
-        ulLeft -= 1387;
+        ulLeft -= 1423;
         // console.log(ulLeft);
         animate(ul, ulLeft);
         ulIndex++;
@@ -38,15 +38,15 @@ window.onload =function(){
         }
         //4.3改变页面  第五张图片结束后ulIndex是4 
         //第六张图片即第一张图片的ulIndex是5  所以求ulIndex % 5恢复ulIndex为0 
-        spanArr[ulIndex % 5].className = 'current';
-    },3000);
+        spanArr[ulIndex % 3].className = 'current';
+    },4000);
     //第二大步  给小圆点span添加点击事件
     for(var i = 0; i< spanArr.length; i++){
         var span = spanArr[i];
         span.index = i;
         span.onclick = function(){
             //实现点击span后图片移动
-            var targetLeft = -1387 * this.index; //0 - 4
+            var targetLeft = -1423 * this.index; //0 - 4
             //点击后自动滚动到 当前圆点对应的图片的位置  即左移800*下标
             animate(ul, targetLeft);
             //记录此时的ulLeft ulIndex 为了继续从当前点击图片向下一张图片移动
