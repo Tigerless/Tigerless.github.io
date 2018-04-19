60f3fd73cc2f3a5f552f899bb431c76789e95dc8
tigerdone <3196083633@qq.com>
2018-04-20 00:09:06 +0800

    animate3

--- js/animate1.js
+++ js/animate1.js
@@ -2,40 +2,143 @@
 
 // var scrollY = 0;
 var angle = 0;
+var animate2_top = 0;
+var animate3_top = 0;
+var animate4_top = 0;
+var codeframe_view = true;
+var content3_code1_view = true;
+var content3_code2_view = true;
 
+var content4_code1_view = true;
+var content4_code2_view = true;
 
 window.onscroll = function() {
 
-    //animate1
+    $(document).ready(function () {
 
-    var scrollY = window.scrollY;
-    console.info(scrollY);
-    if(scrollY < 500){
-        var alpha = 1 - scrollY/400;
-        var width_reduce = scrollY/4*3;
-        var height_reduce = width_reduce/2;
-        var refer_reduce = height_reduce*5/7;//相对点减少度
-        // console.log('h12:'+scrollY);
-        // head animate :506为不动点，1440*717高度
-        if(alpha<0){
-            alpha = 0
+        //animate1
+        var scrollY = window.scrollY;
+        console.info(scrollY);
+        if (scrollY < 500) {
+
+                var alpha = 1 - scrollY / 400;
+                var width_reduce = scrollY / 4 * 3;
+                var height_reduce = width_reduce / 2;
+                var refer_reduce = height_reduce * 5 / 7;//相对点减少度
+                // console.log('h12:'+scrollY);
+                // head animate :506为不动点，1440*717高度
+                if (alpha < 0) {
+                    alpha = 0
+                }
+                $("#main_head").fadeTo(30, alpha);
+                $("#ball_nebula").animate({
+                    left: -86 + width_reduce / 2 + 'px',
+                    top: -10 + refer_reduce + 'px',
+                    width: (1201 - width_reduce) + 'px'
+                }, 20);
+
         }
-        $(document).ready(function(){
-            $("#main_head").fadeTo(30,alpha);
-            $("#ball_nebula").animate({left: -135+width_reduce/2+'px', top: -10 + refer_reduce +'px', width: (1201-width_reduce)+'px'},20);
 
-        });
-    }
+        //animate2
+        angle = scrollY / 1100 * 360;
+        if (scrollY < 1100) {
+            $('.img_bg').rotate(angle);
+            $('#content1_img1').rotate(-angle);
+            $('#content1_img2').rotate(-angle);
+            $('#content1_img3').rotate(-angle);
+            $('#content1_img5').rotate(angle);
+        }
+        //animate3
+        //400--900,1353px
+        if (scrollY < 900 && scrollY > 400) {
+            animate2_top = (scrollY - 400) / 500 * 903
+            // console.log("ojbk:"+the_top)
+            // var code = $("#code");
+            $("#code").animate({
+                top: -animate2_top + "px",
+            }, 5);
 
-    //animate2
-    angle = scrollY/1100*360;
-    if(scrollY < 1100){
-        $('.img_bg').rotate(angle);
-        $('#content1_img1').rotate(-angle);
-        $('#content1_img2').rotate(-angle);
-        $('#content1_img3').rotate(-angle);
-        $('#content1_img5').rotate(angle);
 
-    }
+
+        }
+        //505*372
+        if(scrollY > 900 && codeframe_view == true){
+            $(".codeframe").fadeTo(1000,0.15);
+            codeframe_view = false
+            $("#result").animate({
+                left: '6px',
+                top: '77px',
+                width: '505px',
+                opacity:'1',
+            }, 1000);
+        }
+        if(scrollY < 900 && codeframe_view == false){
+            $(".codeframe").fadeTo(1000,1);
+            codeframe_view = true
+            $("#result").animate({
+                left: '263px',
+                top: '237px',
+                width: '0',
+                opacity:'0',
+            }, 1000);
+        }
+
+        //1117--1917
+        //animate3
+        if(scrollY > 1117 && scrollY < 1917){
+            if(scrollY < 1317){
+                animate3_top = (scrollY - 1117) / 200 * 231
+                $("#content3_code1").animate({
+                    top: -animate3_top + "px",
+                }, 5);
+            }
+            if(scrollY > 1517 && content3_code1_view == true){
+                $("#content3_code1").fadeTo(200,0);
+                content3_code1_view = false
+            }
+            if(scrollY < 1517 && content3_code1_view == false){
+                $("#content3_code1").fadeTo(200,1);
+                content3_code1_view = true
+            }
+
+            if(scrollY > 1717 && content3_code2_view == true){
+                $("#content3_code2").fadeTo(200,0);
+                content3_code2_view = false
+            }
+            if(scrollY < 1717 && content3_code2_view == false){
+                $("#content3_code2").fadeTo(200,1);
+                content3_code2_view = true
+            }
+        }
+
+        //1717--2384
+        //animate4
+        if(scrollY > 1712 && scrollY < 2412){
+
+            if(scrollY > 1837 && content4_code1_view == true){
+                $("#content4_code1").fadeTo(500,0);
+                content4_code1_view = false
+            }
+            if(scrollY < 1837 && content4_code1_view == false){
+                $("#content4_code1").fadeTo(500,1);
+                content3_code1_view = true
+            }
+
+            if(scrollY > 2062 && content4_code2_view == true){
+                $("#content4_code2").fadeTo(500,0);
+                content4_code2_view = false
+            }
+            if(scrollY < 2062 && content3_code2_view == false){
+                $("#content4_code2").fadeTo(500,1);
+                content4_code2_view = true
+            }
+
+            if(scrollY < 2412){
+                animate4_top = (scrollY - 2062) / 175 * 628
+                $("#content4_code3").animate({
+                    top: -animate4_top + "px",
+                }, 5);
+            }
+        }
+    });
 }
-
