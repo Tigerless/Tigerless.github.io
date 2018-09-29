91c089a5923301c965cc1ee9a48f43d4427114c7
tigerdone <3196083633@qq.com>
2018-09-30 01:03:26 +0800

    day9_30

--- js/ex_tab.js
+++ js/ex_tab.js
@@ -1,22 +1,38 @@
 window.onload = function () {
     var mytabs = document.getElementsByClassName("body3_item");
+    var navs = document.getElementsByClassName("header_items");
+
     var mytab = mytabs[0].getElementsByTagName("div");
+    var nav = navs[0].getElementsByTagName("a");
 
     var Paper = document.getElementById("Paper");
     var patent = document.getElementById("patent");
     var copyright = document.getElementById("copyright");
     var Awards = document.getElementById("Awards");
 
-    console.log(mytab.length)
+    console.log("nav:::"+nav.length)
+    for(var i2 = 0;i2<nav.length;i2++) {
+        nav[i2].indexs = i2;
+        nav[i2].onclick = function () {
+
+            for(var j2 = 0;j2<nav.length;j2++){
+                nav[j2].className = "";
+            }
+            nav[this.indexs].className = "choose";
+
+        }
+    }
+
 
     for(var i = 0;i<mytab.length;i++){
         mytab[i].getElementsByTagName("a")[0].indexs = i;
         mytab[i].getElementsByTagName("a")[0].onclick = function () {
+
             for(var j = 0;j<mytab.length;j++){
                 mytab[j].className = "publication_button";
-
             }
             mytab[this.indexs].className = "body3_get";
+
             Paper.className = "display";
             patent.className = "display";
             copyright.className = "display";
